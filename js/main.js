e4bf7b990b4fadd29392420c0100fb780603d159
hhonggm <hhonggm@naver.com>
2021-07-19 21:15:49 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,10 +20,10 @@ $(document).ready(function(){
 			var $tabPanels = $scope.find('.tabpanel'),
 				$ovPanel = $tabPanels.filter('.on')
 			$ovTab.removeClass('on');
-			$ovTab.find('a').attr('aria-selected', 'false');
-			$ovTab.find('a').attr("title","숨김");
+			$ovTab.children('a').attr('aria-selected', 'false');
+			$ovTab.children('a').attr("title","숨김");
 
-			$this.parent('li').addClass('on');
+			$this.parents('li').addClass('on');
 			$this.attr('aria-selected', 'true');
 			$this.attr("title","열림");
 			var href = $this.attr("aria-controls");
